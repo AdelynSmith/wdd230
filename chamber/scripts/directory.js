0a1261bd96a6db4825c9dad3b8cf68601ad679e5
adelynsmith <smi22027@byui.edu>
2023-03-04 14:20:00 -0700

    final css for directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -21,10 +21,12 @@ async function getBusinessData() {
         // Create elements to add to the div.cards element
         let card = document.createElement("section");
         let h2 = document.createElement("h2");
+        let p = document.createElement("p");
         let logo = document.createElement("img");
   
         // Build the h2 content out to show the prophet's full name - finish the template string
-        h2.textContent = `${business.name}`;
+        h2.textContent = `${business.name} `;
+        p.textContent = `${business.address} ${business.phone} ${business.membership}`
   
         // Build the image portrait by setting all the relevant attribute
         logo.setAttribute("src", business.image);
@@ -34,8 +36,9 @@ async function getBusinessData() {
         );
   
         // Append the section(card) with the created elements
-        card.appendChild(h2);
         card.appendChild(logo);
+        card.appendChild(h2);
+        card.appendChild(p);
   
         cards.appendChild(card);
       } // end of forEach loop
@@ -58,7 +61,8 @@ async function getBusinessData() {
       let td_name = document.createElement("td");
       let td_address = document.createElement("td");
       let td_phone = document.createElement("td");
-      let td_website = document.createElement("td")
+      let td_website = document.createElement("td");
+      let td_membership = document.createElement("td");
   
       td_name.textContent = `${business.name}`;
       td_address.textContent = business.address;
