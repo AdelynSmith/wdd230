2bb3e9a6d0d984460bca44812697bb4f60e6db23
adelynsmith <smi22027@byui.edu>
2023-03-29 10:43:59 -0600

    hero

--- bountiful/scripts/main.js
+++ bountiful/scripts/main.js
@@ -24,3 +24,26 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 );
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
+
+// Number of Drinks Ordered
+
+if(!localStorage.getItem('lastvisit')) {
+    localStorage.setItem('lastvisit', Date.now());
+    document.getElementById('number_ordered').textContent = 'This is your 1st order';
+} else {
+    drinksOrdered();
+}
+
+function drinksOrdered() {
+    let lastDate = localStorage.getItem('lastvisit');
+    let currDate = Date.now();
+    
+    let difference = currDate - lastDate;
+        console.log(difference);
+        let daysDifference = Math.floor(difference/1000/60/60/24);
+
+    document.getElementById('number_ordered').textContent = daysDifference;
+
+    localStorage.setItem('lastvisit', Date.now());
+
+}
