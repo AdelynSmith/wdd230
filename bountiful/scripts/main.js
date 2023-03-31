bb2ddf2708ee8fc91ad550b1f7749eca30e77760
adelynsmith <smi22027@byui.edu>
2023-03-30 18:58:41 -0600

    javascript

--- bountiful/scripts/main.js
+++ bountiful/scripts/main.js
@@ -27,23 +27,64 @@ datefield.innerHTML = `<em>${fulldate}</em>`;
 
 // Number of Drinks Ordered
 
-if(!localStorage.getItem('lastvisit')) {
-    localStorage.setItem('lastvisit', Date.now());
-    document.getElementById('number_ordered').textContent = 'This is your 1st order';
-} else {
-    drinksOrdered();
+// drinkCount();
+let drinkcount = 0
+function drinkCount() {
+    console.log(localStorage.getItem('drinkcount'))
+  if (localStorage.getItem('drinkcount')) {
+    drinkcount = Number(localStorage.getItem('drinkcount'))+1;
+    localStorage.setItem('drinkcount', drinkcount);
+  } else {
+    localStorage.setItem('drinkcount', 1);
+  }
+  console.log(localStorage.getItem('drinkcount'));
+  document.querySelector("#number_ordered").innerHTML = localStorage.getItem('drinkcount');
 }
+document.querySelector("#drinkCount").addEventListener("click", drinkCount)
 
-function drinksOrdered() {
-    let lastDate = localStorage.getItem('lastvisit');
-    let currDate = Date.now();
-    
-    let difference = currDate - lastDate;
-        console.log(difference);
-        let daysDifference = Math.floor(difference/1000/60/60/24);
+// Fruit options
+const list = document.querySelector('ul');
+      const input = document.querySelector('input');
+      const button = document.querySelector('button');
 
-    document.getElementById('number_ordered').textContent = daysDifference;
+      button.addEventListener('click', () => {
+        const myItem = input.value;
+        input.value = '';
 
-    localStorage.setItem('lastvisit', Date.now());
+        const listItem = document.createElement('li');
+        const listText = document.createElement('span');
+        const listBtn = document.createElement('button');
 
-}
+        listItem.appendChild(listText);
+        listText.textContent = myItem;
+        listItem.appendChild(listBtn);
+        listBtn.textContent = 'X';
+        list.appendChild(listItem);
+
+        listBtn.addEventListener('click', () => {
+          list.removeChild(listItem);
+        });
+
+        input.focus();
+      });
+
+
+      // Order message
+      // get the feedback div element so we can do something with it.
+const feedbackElement = document.getElementById("fresh_content");
+// get the form so we can read what was entered in it.
+const formElement = document.forms[0];
+// add a 'listener' to wait for a submission of our form. When that happens run the code below.
+formElement.addEventListener("submit", function (e) {
+  // stop the form from doing the default action.
+  e.preventDefault();
+  // set the contents of our feedback element to a message letting the user know the form was submitted successfully. Notice that we pull the name that was entered in the form to personalize the message!
+  feedbackElement.innerHTML =
+    "Hello " +
+    formElement.firstname.value +
+    "! You ordered: ";
+  // make the feedback element visible.
+  feedbackElement.style.display = "block";
+  // add a class to move everything down so our message doesn't cover it.
+  document.body.classList.toggle("moveDown");
+});
